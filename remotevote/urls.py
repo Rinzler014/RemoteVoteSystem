63584e8a8298455d08e13e4a50b24929c6049312
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2022-12-06 21:00:34 -0600

    Added Admin Portal

--- remotevote/urls.py
+++ remotevote/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "CEE Administrator"
+admin.site.site_title = "CEE Admin Portal"
+admin.site.index_title = "Portal de Administración CEE"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("census.urls")),
